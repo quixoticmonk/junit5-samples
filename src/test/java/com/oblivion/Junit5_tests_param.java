45309ba0c98921134bdd20fc52e1d3392c1717e1
devopsuser25 <manu.chandrasekhar@accenture.com>
2019-10-11 11:21:23 -0400

    Initial commit

---
+++ src/test/java/com/oblivion/Junit5_tests_param.java
@@ -0,0 +1,59 @@
+package com.oblivion;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.time.Month;
+import java.util.EnumSet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("The Tests")
+public class Junit5_tests_param {
+
+    @Test
+    @DisplayName("should Return True")
+    void shouldReturnTrue(){
+        assertTrue(true);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings={"Manu","Tahiti"})
+    @DisplayName("for different parameters")
+    void shouldCheckDiffParameters(String str){
+        assertTrue(str.contains("a"));
+    }
+
+//Reg expressions
+    @ParameterizedTest(name = "{index} {0} ends in ber")
+    @EnabledOnOs({OS.LINUX})
+    @EnumSource(value = Month.class, names = ".+BER", mode = EnumSource.Mode.MATCH_ANY)
+    void fourMonths_AreEndingWithBer(Month month) {
+        EnumSet<Month> months =
+                EnumSet.of(Month.SEPTEMBER, Month.OCTOBER, Month.NOVEMBER, Month.DECEMBER);
+        assertTrue(months.contains(month));
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({"test,TEST", "tEst,TEST", "Java,JAVA"})
+    void toUpperCase_ShouldGenerateTheExpectedUppercaseValue(String input, String expected) {
+        String actualValue = input.toUpperCase();
+        assertEquals(expected, actualValue);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"test:test", "tEst:test", "Java:java"}, delimiter = ':')
+    void toLowerCase_ShouldGenerateTheExpectedLowercaseValue(String input, String expected) {
+        String actualValue = input.toLowerCase();
+        assertEquals(expected, actualValue);
+    }
+
+}
